7a506ae481141157e66a44c5d4d8ab48bd3eeb9e
Nataliia Cheban <n.cheban@gmail.com>
2021-07-01 20:48:01 +0200

    First commit

---
+++ timec.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int hourin;
+	int minin;
+	int hourout;
+	int hourout;	
+	
+	printf("Введите часы:%d",hourin);
+	printf("Введите минуты:%d\n",minin);
+
+	return 0;
+}
