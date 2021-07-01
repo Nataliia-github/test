51111cf54ce2fe1dd0a461c12c12c721ff0c19a5
Nataliia Cheban <n.cheban@gmail.com>
2021-07-01 21:55:04 +0200

    Wrong time

--- timec.c
+++ timec.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(){
-	int hourin;
-	int minin;
-	int hourout;
-	int hourout;	
-	
-	printf("Введите часы:%d",hourin);
-	printf("Введите минуты:%d\n",minin);
 
-	return 0;
+	clock_t start_t;
+
+	start_t = clock();
+   	printf("Сейчас в Амстердаме, start_t = %ld\n", start_t);
+    	return 0;	
+
 }
