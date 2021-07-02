2bacb4c34ce6c8e94ac008b1948743edfed4b4ed
Nataliia Cheban <n.cheban@gmail.com>
2021-07-02 19:49:56 +0200

    Working programm input output. Quit is CTRL+D

--- inout.c
+++ inout.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main(){
-
+	
 	int c;
 	c = getchar();
-	while (c !=EOF){
-	putchar (c);
-	c = getchar ();
-	}
+	
+	while (c != EOF){
+		putchar (c);
+		c = getchar ();
+		}	
 }
