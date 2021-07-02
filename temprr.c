02e0ad19ed8ed77e45d9e315ad1c4ae0d129130f
Nataliia Cheban <n.cheban@gmail.com>
2021-07-02 10:11:01 +0200

    Symbolic Constants

--- temprr.c
+++ temprr.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
 
 int main (){
   
 	int farh;
-	farh = 300;
 
-	for (farh =300; farh >= 0; farh = farh - 20){
+	for (farh = UPPER; farh >= LOWER; farh = farh - STEP){
 	printf ("%10d%10.1f\n",farh, (5.0/9.0)*(farh-32));
 	}
 }
