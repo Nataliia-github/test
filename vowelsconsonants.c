39dc0cd701824c232ead3d447c0e8b73be0cec97
Nataliia Cheban <n.cheban@gmail.com>
2021-07-10 10:17:50 +0200

    "Settings Visual Studio"

---
+++ vowelsconsonants.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+	char letter;
+	char vowels[] = {'a','e','i','o','u'};
+	int is_consonant = 1;
+	printf ("Input letter for check: ");
+	scanf ("%c", &letter);
+
+
+	for (i = 0;i < sizeof(vowels); ++i){
+		if (letter == vowels[i]){
+			is_consonant = 0;
+		}
+	}
+
+	if (is_consonant == 1) {
+		printf ("The letter is consonant.\n");
+	} else {
+		printf ("The letter is vowels.\n");
+	}	 
+	return 0;
+}
