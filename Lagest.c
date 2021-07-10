00b41a02a09ab426a17642e7fe87a8c2bdef3019
Nataliia Cheban <n.cheban@gmail.com>
2021-07-10 13:34:58 +0200

    Lagest number

--- Lagest.c
+++ Lagest.c
@@ -4,23 +4,26 @@ int main (){
 
     int a, b, c, lagest;
 
-    printf("Input three integers:\n First one is: ");
+    printf("Input three integers:\nN 1 = ");
     scanf ("%d", &a);
-    printf("Second one is: ");
+    printf("N 2 = ");
     scanf ("%d", &b);
-    printf("Third one is: ");
-    scanf ("%d", &a);
+    printf("N 3 = ");
+    scanf ("%d", &c);
 
-    if ( a > b && a > c){
+    if (a > b && a > c){
         lagest = a;
     }
-        else if (b > c && b > a) {
+    if (b > a && b > c){
         lagest = b;
-        }
-            else
-            {
-                lagest = c;
-            }
-    printf("The lagest number is: %d", lagest);
-
+    }
+    if (c > a && c > b){
+        lagest = c;
+    }
+    if (lagest == 0){
+        printf("They are some equal integers.\n");
+    }
+    else 
+        printf ("The lagest number is: %d\n", lagest);
+    return 0;
 }
