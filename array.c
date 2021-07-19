f6a5153a098436e3728bb22e52f56e81688871f0
Nataliia Cheban <n.cheban@gmail.com>
2021-07-19 16:25:13 +0200

    Array 1

--- array.c
+++ array.c
@@ -4,16 +4,23 @@ int main () {
 
 int values [5];
 int i;
+int sum = 0, avarege = 0;
     
     for (i = 0; i < 5; ++i){
         scanf("%d", &values[i]);
+        sum = sum + values[i];
     }
 
+    avarege = sum / i;
     printf("Display integers: \n");
-
+  
     for (i = 0; i < 5; ++i)
     {
         printf("%d\n", values[i]);
     }
+
+    printf("Display avarege: %d\n", avarege);
+
     return 0;
 }
+
